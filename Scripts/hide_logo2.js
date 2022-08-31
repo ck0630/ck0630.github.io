834e6f805a3f4e3d1236c3f84cc45daf393f8e7b
ck0630 <ck0630@gmail.com>
2022-08-31 10:04:12 +0800

    Update hide_logo2.js

--- Scripts/hide_logo2.js
+++ Scripts/hide_logo2.js
@@ -5,7 +5,7 @@
 
 function check_taiwan() {
   var apiKey = '41b35af8793c40819cf4cf013b54802c';
-  var url = 'https://ipgeoloc' + 'ation.abstra' + ctapi.com/v1/?api_key=' + apiKey + '&fields=country';
+  var url = 'https://ipgeoloc' + 'ation.abstra' + 'ctapi.com/v1/?api_key=' + apiKey + '&fields=country';
   $.getJSON(url, function(data) {
     if (data.country=='Taiwan') {
        $("div[style*='display:block !important']").hide();
