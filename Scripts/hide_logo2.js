70996bf6500cf010b60786693ee1f39b26ce2a09
ck0630 <ck0630@gmail.com>
2022-08-31 10:00:34 +0800

    Update hide_logo2.js

--- Scripts/hide_logo2.js
+++ Scripts/hide_logo2.js
@@ -3,7 +3,7 @@
 //}
 //$("div:contains('condemn war.')").hide();
 
-function check_taiwan {
+function check_taiwan() {
   var apiKey = '41b35af8793c40819cf4cf013b54802c';
   var url = 'https://ipgeoloc' + 'ation.abstra' + ctapi.com/v1/?api_key=' + apiKey + '&fields=country';
   $.getJSON(url, function(data) {
