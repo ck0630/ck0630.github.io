a6b0aeb7a37ce09ded8c6541c91862febbe83d74
ck0630 <ck0630@gmail.com>
2022-08-31 09:49:48 +0800

    Add files via upload

---
+++ Scripts/hide_logo2.js
@@ -0,0 +1,23 @@
+﻿//if (is_chrome_android) {
+//  $("div[style*='display:block !important']").hide();
+//}
+//$("div:contains('condemn war.')").hide();
+
+function check_taiwan {
+  var apiKey = '41b35af8793c40819cf4cf013b54802c';
+  var url = 'https://ipgeoloc' + 'ation.abstra' + ctapi.com/v1/?api_key=' + apiKey + '&fields=country';
+  $.getJSON(url, function(data) {
+    if (data.country=='Taiwan') {
+       $("div[style*='display:block !important']").hide();
+    }
+  });   
+  
+}
+
+
+
+
+
+
+
+
